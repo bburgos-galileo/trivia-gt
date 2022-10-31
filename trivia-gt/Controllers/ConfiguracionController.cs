fa26fdaf857f1a798c5425b66fd50d17095b2326
Byron Burgos <bburgos@plusti.com>
2022-10-31 14:17:15 -0600

    se agregó validacion de url

--- trivia-gt/Controllers/ConfiguracionController.cs
+++ trivia-gt/Controllers/ConfiguracionController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 using trivia_gt.DAL;
 using trivia_gt.Models;
 
@@ -21,9 +22,9 @@ namespace trivia_gt.Controllers
                 return Redirect("/Login/Login");
             }
 
-            _lista = configuracionDAL.Listar( new ConfiguracionBE());
+            _lista = configuracionDAL.Listar(new ConfiguracionBE());
 
-            if(_lista.Count > 0)
+            if (_lista.Count > 0)
             {
                 configuracionBE = _lista[0];
             }
@@ -43,7 +44,23 @@ namespace trivia_gt.Controllers
         public IActionResult Editar(ConfiguracionBE entidad)
         {
             ConfiguracionDAL configuracionDAL = new ConfiguracionDAL();
-            
+
+            if (!IsWebsiteUp_Get(entidad.urlApi))
+            {
+                ViewBag.Nombres = HttpContext.Session.GetString("Nombres");
+                ViewBag.IdRol = HttpContext.Session.GetInt32("IdRol");
+                ViewBag.Imagen = @"https://drive.google.com/uc?export=view&id=" + HttpContext.Session.GetString("Imagen");
+                ViewBag.Visible = false;
+                ViewBag.Nivel = 0;
+                ViewBag.Percentage = 0;
+                ViewBag.ImagenCombo = @"https://drive.google.com/uc?export=view&id=1wQ3L1xIfvyfoUueYKik5GTNTM1tYU89w";
+
+                ModelState.AddModelError("urlApi", "La url ingresada no es valida");
+
+                return View(entidad);
+
+            }
+
             if (entidad.idConfiguracion == 0)
             {
                 configuracionDAL.Crear(entidad);
@@ -55,5 +72,23 @@ namespace trivia_gt.Controllers
 
             return Redirect("/Home/Index");
         }
+
+        private bool IsWebsiteUp_Get(string url)
+        {
+            url += "1&grupo=1";
+
+            try
+            {
+
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage httpResponse = httpClient.GetAsync(url).Result;
+                return httpResponse.IsSuccessStatusCode;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
         }
+    }
 }
